c270e7739bacb94ed106ffa7a335776c807aa700
ConsFontaniliLuca <cons.fontanililuca@sia.eu>
2016-11-09 22:11:56 +0100

    Added Facade Pattern

---
+++ lucafontanili.designpatterns.python/src/structural/facade/FacadeClient.py
@@ -0,0 +1,9 @@
+'''
+Created on Nov 9, 2016
+
+@author: Admin
+'''
+from ImageFacade import *
+
+facade = ImageFacadeClass()
+facade.zip_image('sunshine.png', 'sunshine.zip')
---
+++ lucafontanili.designpatterns.python/src/structural/facade/ImageFacade.py
@@ -0,0 +1,30 @@
+'''
+Created on Nov 9, 2016
+
+@author: Admin
+'''
+
+class ImageCompressor:
+    
+    @staticmethod
+    def compress_image(image_name, output_file):
+        print('Compressing image ' + image_name + ' to ' + output_file) 
+    
+class ImageReader:
+
+    @staticmethod    
+    def read_image(image_name):
+        print('Reading image ' + image_name)
+        return image_name
+
+class ImageFacadeClass:
+    
+    _instances = {}
+
+    def __new__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(ImageFacadeClass, cls).__new__(cls, *args, **kwargs)
+        return cls._instances[cls]
+    
+    def zip_image(self, image_name, ouptut_file):
+        ImageCompressor.compress_image(ImageReader.read_image(image_name), ouptut_file)
