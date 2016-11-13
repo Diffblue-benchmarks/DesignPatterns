9eb66d05d228615f4588eaf4e896a382417aa65a
ConsFontaniliLuca <cons.fontanililuca@sia.eu>
2016-11-13 18:47:22 +0100

    Added Observer Pattern

--- lucafontanili.designpatterns.python/src/behavioral/observer/ObserverClient.py
+++ lucafontanili.designpatterns.python/src/behavioral/observer/ObserverClient.py
@@ -7,10 +7,14 @@ Created on Nov 13, 2016
 from ClockTimer import *
 import time
 
-clock_timer = ClockTimer()
-clock_timer.add_observer(ClockObserver12())
-clock_timer.add_observer(ClockObserver24())
-
-for i in range(0,5):
-    clock_timer.tick()
-    time.sleep(1)
+def main():
+    clock_timer = ClockTimer()
+    clock_timer.add_observer(ClockObserver12())
+    clock_timer.add_observer(ClockObserver24())
+    
+    for i in range(0,5):
+        clock_timer.tick()
+        time.sleep(1)
+        
+if __name__ == '__main__':
+    main()
